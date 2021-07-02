9e826cbe23cbba06e4c8dfed1e90a4fa10dbd1ed
hari krishnan <harikrishnan9a@gmail.com>
2021-07-02 10:30:25 -0500

    ghgv

--- README.md
+++ README.md
@@ -1,84 +1,24 @@
-# WebRTC Video Chat
+# ZoomCord
+Google Meet / Zoom / Discord clone
 
-Simple Web Application that offer you to create video meeting room using WebRTC and Socket
+A web application for video meeting + realtime text chat.
+Developed with Node.js, Express, Peer.js and Socket.io.
 
-## Getting Started
+## Features
+- [x] Join and leave room
+- [x] Group video meeting
+- [x] Group text chat
+- [x] Unique username checking
+- [x] 'User is typing...'
+- [ ] Limit number of users per room
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
-
-### Prerequisites
-
-You have to install [Node.js](https://nodejs.org/en/) in your machine.
-
-### Installing
-
-After installing node clone the repo by using git
-
-```
-git clone https://github.com/50UM3N/video-chat.git
-                        or
-git clone git@github.com:50UM3N/video-chat.git
-```
-
-Or you can download the zip file.
-
-Then open cmd or git bash on the project folder to install some modules that I used to build this project
-
-Install Once
-
+## Usage
 ```
 npm install
+npm run dev
+
+npm i -g peer
+peerjs --port 3001
+
+Go to localhost:3000
 ```
-
-[Nodemon](https://www.npmjs.com/package/nodemon) For automatically restart the server as a dev dependency (optional)
-
-```
-npm i --sav-dev nodemon
-```
-
-## Setting Database
-
-For the database I use [mongodb](https://www.mongodb.com/) [Atlas](https://www.mongodb.com/cloud/atlas). But you can use the local also.
-
-If you don't have any account in [Atlas](https://www.mongodb.com/cloud/atlas). Visit the page and create a free account then get your connection string. More at [Documentation](https://docs.atlas.mongodb.com/tutorial/create-new-cluster/).
-
-Rename the file .env_sample to .env then replace "connection string" to your connection string
-
-Example
-
-```
-MONGO_URL=mongodb+srv://dbUser:<password>@cluster0.kfx8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-```
-
-## Running the App
-
-If you install nodemon the you can use. (devStart script is already added to the package.json)
-
-```
-npm run devStart
-```
-
-or
-
-```
-node server.js
-```
-
-## Live Demo
-
-For deploy the project I use [heroku](https://heroku.com)
-
-[Video Chat](https://soumen-video-calling-app.herokuapp.com/)
-
-## Built With
-
--   [Node Js](https://nodejs.org/en/) - The Backend
--   [Peer JS](https://peerjs.com/) - PeerJS simplifies WebRTC peer-to-peer data, video, and audio calls.
--   [SocketIo](https://socket.io/) - For realtime communication
--   [NPM](https://www.npmjs.com/) - Dependency Management
--   [GIT](https://git-scm.com/) - Used for version control
--   [Heroku](https://heroku.com) - Used to Deploy Node.js applications
-
-## License
-
-Video Chat is released under the [MIT license](LICENSE.txt).
--- server.js
+++ server.js
@@ -1,147 +1,103 @@
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-const { ExpressPeerServer } = require("peer");
-const cookie = require("cookie-session");
-const passport = require("passport");
-const flash = require("express-flash");
-const mongoose = require("mongoose");
-const passportAuthenticator = require("./functions/passportStrategy");
-const user = require("./schema/user");
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-});
-const peerUser = require("./schema/peerUser");
-const room = require("./schema/rooms");
+const server = require('http').createServer(app);
+const io =  require('socket.io')(server);
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
-const videoRoom = require("./routes/video");
-const signup = require("./routes/auth/signup");
-const login = require("./routes/auth/login");
-const logout = require("./routes/auth/logout");
-const index = require("./routes/index");
-const newMeeting = require("./routes/newMeeting");
-mongoose
-    .connect("mongodb+srv://hari:123@sample.rfsqu.mongodb.net/Meet?retryWrites=true&w=majority", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    })
-    .then(() => {
-        console.log("database connected");
-    });
-passportAuthenticator(passport, user);
-app.use(express.json());
-app.use("/peerjs", peerServer);
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
-app.use(cookie({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: ["soumenkhara"] }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static("public"));
-app.use(flash());
-app.use(require("express-ejs-layouts"));
-app.set("layout", "layouts/layout");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
-app.post("/join-room", (req, res) => {
-    res.redirect(`/${req.body.room_id}`);
-});
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
 
-// index route
-app.use("/", index);
+const botName = 'ZoomCord Bot';
+const ACCEPTED_ROOMS = ["Malaysia", "Indonesia", "Singapore"];
 
-// user id get
-app.get("/user", async (req, res) => {
-    res.json({
-        user: await peerUser.findOne({ peerId: req.query.peer }).exec(),
-    });
-});
-// new meeting
-app.use("/new-meeting", newMeeting);
+// Run when client connets
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ userPeerId, username, room }) => {
+        if (!ACCEPTED_ROOMS.includes(room)) {
+            socket.emit('roomNotValid');
+        }
 
-// login
-app.use("/login", login);
+        const user = userJoin(userPeerId, username, room);
 
-// signup
-app.use("/signup", signup);
+        if (!user) {
+            socket.emit('sameName');
+        } else {
 
-// logout
-app.use("/logout", logout);
+            socket.join(user.room);
+        
+            // only show in client to the user connecting
+            socket.emit('message', formatMessage(botName, 'Welcome to ZoomCord!'));
 
-// video room
-app.use("/", videoRoom);
-
-io.on("connection", (socket) => {
-    socket.on(
-        "join-room",
-        async (roomId, peerId, userId, name, audio, video) => {
-            // add peer details
-            await peerUser({
-                peerId: peerId,
-                name: name,
-                audio: audio,
-                video: video,
-            }).save();
-            // add room details
-            var roomData = await room.findOne({ roomId: roomId }).exec();
-            if (roomData == null) {
-                await room({
-                    roomId: roomId,
-                    userId: userId,
-                    count: 1,
-                }).save();
-                roomData = { count: 0 };
-            } else if (roomData.userId == userId) {
-                await room.updateOne(
-                    { roomId: roomId },
-                    { count: roomData.count + 1 }
+            // Broadcast to all except the user itself in a specif room
+            socket.broadcast
+                .to(user.room)
+                .emit(
+                    'message',
+                    formatMessage(botName, `${user.username} has joined the chat`)
                 );
-            }
-            socket.join(roomId);
-            socket
-                .to(roomId)
-                .broadcast.emit(
-                    "user-connected",
-                    peerId,
-                    name,
-                    audio,
-                    video,
-                    roomData.count + 1
-                );
-            socket.on("audio-toggle", async (type) => {
-                await peerUser.updateOne({ peerId: peerId }, { audio: type });
-                socket
-                    .to(roomId)
-                    .broadcast.emit("user-audio-toggle", peerId, type);
+
+            socket.broadcast.to(user.room).emit('user-connected', userPeerId)
+
+            // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room) // E.g return: [{id: '6JhtU8cQGMZzzzj5AAAB', username: 'kaiimran', room: 'Malaysia'}]
             });
-            socket.on("video-toggle", async (type) => {
-                await peerUser.updateOne({ peerId: peerId }, { video: type });
-                socket
-                    .to(roomId)
-                    .broadcast.emit("user-video-toggle", peerId, type);
+
+            socket.on('typing', () => {
+                console.log('typing');
+
+                socket.broadcast
+                    .to(user.room)
+                    .emit('typing', {
+                        username: user.username
+                    });
             });
-            // chat
-            socket.on("client-send", (data) => {
-                socket.to(roomId).broadcast.emit("client-podcast", data, name);
+
+            socket.on('stop typing', () => {
+                console.log('stop typing');
+
+                socket.broadcast
+                    .to(user.room)
+                    .emit('stop typing', {
+                        username: user.username
+                    });
             });
-            socket.on("disconnect", async () => {
-                roomData = await room.findOne({ roomId: roomId }).exec();
-                await room.updateOne(
-                    { roomId: roomId },
-                    { count: roomData.count - 1 }
-                );
-                // remove peer details
-                await peerUser.deleteOne({ peerId: peerId });
-                socket
-                    .to(roomId)
-                    .broadcast.emit(
-                        "user-disconnected",
-                        peerId,
-                        roomData.count - 1
-                    );
+
+            // Listen for chatMessage
+            socket.on('chatMessage', msg => {
+                const user = getCurrentUser(userPeerId);
+
+                // Broadcast to all clients in the room
+                io
+                    .to(user.room)
+                    .emit('message', formatMessage(user.username, msg));
+            });
+
+            // Runs when client disconnects
+            socket.on('disconnect', () => {
+                const user = userLeave(userPeerId);
+
+                if (user) {
+                    io
+                    .to(user.room)
+                    .emit('message', formatMessage(botName, `${user.username} has left the chat`));
+                }
+
+                // Send users and room info
+                io.to(user.room).emit('roomUsers', {
+                    room: user.room,
+                    users: getRoomUsers(user.room)
+                });
+
+                socket.broadcast.to(user.room).emit('user-disconnected', userPeerId)
             });
         }
-    );
+    });
+
 });
-server.listen(process.env.PORT || 3000);
